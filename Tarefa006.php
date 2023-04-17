c422eb8d363e0a409230c69f7be38325d400a3a7
Natalie Marques <namqsf@gmail.com>
2023-04-17 16:37:26 -0300

    Listar Disciplinas via manipulação de arquivos

--- Tarefa006.php
+++ Tarefa006.php
@@ -21,8 +21,21 @@
         <th style="border: 3px solid"><?php echo  $cabecalho[1] ?></th>
         <th style="border: 3px solid"><?php echo  $cabecalho[2] ?></th>
     </tr>
-    <tr>
-
-    </tr>
+    <?php
+        while (!feof($arqDisc)) //enquanto nao chegar ao fim do arquivo...
+        { 
+            $linha = fgets($arqDisc); //le linha por linha e atribui à variavel
+            $dado = explode(";", $linha); // pega a linha e divide em um array de 3 itens
+            echo "<tr>"; //começa outra linha da tabela
+            echo "<td style='border: 3px solid'>" . $dado[0] . "</td>"; //coloca o primeiro dado, o nome
+            echo "<td style='border: 3px solid'>" . $dado[1] . "</td>"; // coloca o segundo dado, a sigla
+            echo "<td style='border: 3px solid'>" . $dado[2] . "</td>"; //coloca o terceiro dado, a carga
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <?php
+    fclose($arqDisc);
+    ?>
 </body>
 </html>
