1e51dd00b6fb86a8de5a988ccc11c4b68a261e8e
Natalie Marques <namqsf@gmail.com>
2023-05-03 08:31:34 -0300

    Add files via upload

---
+++ Tarefa007/Listar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lista de Cadastros</title>
+</head>
+<body>
+    <h1>Listar Discilinas</h1>
+    <?php
+    $arqDisc = fopen("dados.txt", "r") or die("erro ao criar arquivo");
+    echo fgets($arqDisc);
+    $cabecalho = explode(";", fgets($arqDisc));
+    echo $cabecalho[0];
+    echo "<br>";
+    echo fgets($arqDisc);
+    ?>
+<br>
+<table style="border: 3px solid">
+    <tr>
+        <th style="border: 3px solid"><?php echo  $cabecalho[0] ?></th>
+        <th style="border: 3px solid"><?php echo  $cabecalho[1] ?></th>
+        <th style="border: 3px solid"><?php echo  $cabecalho[2] ?></th>
+    </tr>
+    <?php
+        while (!feof($arqDisc)) //enquanto nao chegar ao fim do arquivo...
+        { 
+            $linha = fgets($arqDisc); //le linha por linha e atribui à variavel
+            $dado = explode(";", $linha); // pega a linha e divide em um array de 3 itens
+            echo "<tr>"; //começa outra linha da tabela
+            echo "<td style='border: 3px solid'>" . $dado[0] . "</td>"; //coloca o primeiro dado, o nome
+            echo "<td style='border: 3px solid'>" . $dado[1] . "</td>"; // coloca o segundo dado, o cpf
+            echo "<td style='border: 3px solid'>" . $dado[2] . "</td>"; //coloca o terceiro dado, a matricula
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <?php
+    fclose($arqDisc);
+    ?>
+</body>
+</html>
