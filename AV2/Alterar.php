b2709ec3157aae0a8b145e8f8fcba770ba46790e
Natalie Marques <namqsf@gmail.com>
2023-07-07 22:06:22 -0300

    Update Alterar.php

--- AV2/Alterar.php
+++ AV2/Alterar.php
@@ -14,14 +14,15 @@
         $email = $_GET["email"]; 
         $cargo = $_GET["cargo"]; 
         $sala = $_GET["sala"]; 
-        $salaAlt = $_GET["salaAlt"]
+        $salaAlt = $_GET["salaAlt"];
 
         $conn = new mysqli ($servidor, $user, $pass, $banco);
-        $sql="UPDATE `selecao` SET `nome`='$nome',`cpf`='$cpf',`id`='$id',`email`=$email,`cargo`='$cargo',`sala`='$salaAlt', WHERE `sala` = '$sala'";
+        $sql="UPDATE `selecao` SET `nome`='$nome',`cpf`='$cpf',`id`='$id',`email`='$email',`cargo`='$cargo',`sala`='$salaAlt' WHERE `sala` = '$sala'";
         
-        $result=$conn->query($sql);
+        $result = $conn->query($sql);
         echo $sql;
     } else {
         $ehGET = false;
     }
+?>
 ?>
